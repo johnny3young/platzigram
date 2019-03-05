d5b80201874554522deb23eb3443b5e51d6028d9
unknown <youngerblack@gmail.com>
2019-03-04 23:06:29 -0500

    Otro proyecto

---
+++ app/src/main/java/com/example/platzigram/view/CreateAccountActivity.java
@@ -0,0 +1,15 @@
+package com.example.platzigram.view;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.platzigram.R;
+
+public class CreateAccountActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_account);
+    }
+}
